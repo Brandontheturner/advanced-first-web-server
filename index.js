9122ab19ecc81e4803cf9fe110af72f2e366571a
Brandon Turner <brandontheturner@gmail.com>
2019-04-04 20:49:54 -0500

    learning express commands

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+const state = require("./state");
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+app.post("/users", (req, res) => {
+  state.users.push({
+    _id: 6,
+    name: "David CopperField",
+    occupation: "Street Magician",
+    avatar: "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+  });
+  res.send(state.users[state.users.length - 1]);
+});
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.get("/users", (req, res) => res.send(state.users));
+
+app.get("/users/1", (req, res) => res.send(state.users[0]));
+
+app.put("/users/1", (req, res) => {
+  state.users[0]["name"] = "Micheal Jordan";
+  res.json(state.users[0]);
+});
+
+app.delete("/users/1", (req, res) => {
+  state.users[0]["name"] = "";
+  res.send("deleted the item");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
